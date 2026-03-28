768f0cb861ce48f9e269ae6d0030c2f98523addc
alouirahma462-wq <alouirahma462@gmail.com>
2026-03-28 20:55:42 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { Provider } from 'react-redux'
-import 'core-js'
-
+import ReactDOM from 'react-dom/client'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
-import store from './store'
 
-createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <HashRouter>
     <App />
-  </Provider>,
+  </HashRouter>
 )
+
+
