7b75e00d0b62fb7611f5f41ed4e4fa3b76a533cb
alouirahma462-wq <alouirahma462@gmail.com>
2026-03-28 22:16:51 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,49 +1,9 @@
-import React, { Suspense, useEffect } from 'react'
-import { Route, Routes } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-
-import { CSpinner, useColorModes } from '@coreui/react'
-import './scss/style.scss'
-import './scss/examples.scss'
-
-const DefaultLayout = React.lazy(() => import('./layout/DefaultLayout'))
-
-const Login = React.lazy(() => import('./views/pages/login/Login'))
-
-
-const Register = React.lazy(() => import('./views/pages/register/Register'))
-const Page404 = React.lazy(() => import('./views/pages/page404/Page404'))
-const Page500 = React.lazy(() => import('./views/pages/page500/Page500'))
-
-const App = () => {
-  const { isColorModeSet, setColorMode } = useColorModes(
-    'coreui-free-react-admin-template-theme'
-  )
-
-  const storedTheme = useSelector((state) => state.theme)
-
-  useEffect(() => {
-    const urlParams = new URLSearchParams(window.location.href.split('?')[1])
-    const theme = urlParams.get('theme')?.match(/^[A-Za-z0-9\s]+/)?.[0]
-
-    if (theme) setColorMode(theme)
-    if (isColorModeSet()) return
-
-    setColorMode(storedTheme)
-  }, [])
-
+export default function App() {
   return (
-    <Suspense fallback={<div className="pt-3 text-center"><CSpinner /></div>}>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/404" element={<Page404 />} />
-        <Route path="/500" element={<Page500 />} />
-        <Route path="*" element={<DefaultLayout />} />
-      </Routes>
-    </Suspense>
+    <div style={{ padding: 40 }}>
+      <h1>النظام شغال ✔</h1>
+    </div>
   )
 }
 
-export default App
 
