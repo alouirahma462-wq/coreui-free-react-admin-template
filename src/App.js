85bb03a3a7200e42e9ad012b8092158831124939
alouirahma462-wq <alouirahma462@gmail.com>
2026-03-28 21:53:28 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ import './scss/examples.scss'
 
 const DefaultLayout = React.lazy(() => import('./layout/DefaultLayout'))
 
-const Login = React.lazy(() => import('./views/pages/login/Login'))
+const Login = React.lazy(() => import('./Login'))
+
 const Register = React.lazy(() => import('./views/pages/register/Register'))
 const Page404 = React.lazy(() => import('./views/pages/page404/Page404'))
 const Page500 = React.lazy(() => import('./views/pages/page500/Page500'))
