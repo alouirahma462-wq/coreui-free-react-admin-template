93afef4081ebcf5924a33f5915fc00a3626c2d33
alouirahma462-wq <alouirahma462@gmail.com>
2026-03-31 16:53:23 +0100

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
+// 🔥 ضع بيانات مشروعك هنا
 const supabaseUrl = "https://wdjssmvfwzpahhnifihd.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndkanNzbXZmd3pwYWhobmlmaWhkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ3OTg5MTIsImV4cCI6MjA5MDM3NDkxMn0.2zwHzfppzufjL1T9GHbJSUhve2dwh92Lk-uEdb5mKMk";
+
+// ⚠️ مهم جداً: استخدم Publishable Key فقط (مو secret)
+const supabaseKey = "sb_publishable_aEshRguohYW3XKWQktNnRg__NIDQVef";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
+
 
