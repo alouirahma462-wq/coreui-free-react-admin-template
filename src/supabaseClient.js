74979c86aaef3a90d28de9fc74501641884b5933
alouirahma462-wq <alouirahma462@gmail.com>
2026-03-31 15:54:07 +0100

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://wdjssmvfwzpahhnifihd.supabase.co"
+const supabaseUrl = "https://wdjssmvfwzpahhnifihd.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndkanNzbXZmd3pwYWhobmlmaWhkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ3OTg5MTIsImV4cCI6MjA5MDM3NDkxMn0.2zwHzfppzufjL1T9GHbJSUhve2dwh92Lk-uEdb5mKMk";
 
-const supabaseKey = "sb_publishable_CsYZU2031CakNqTp3X121A_fBYKamBE"
+export const supabase = createClient(supabaseUrl, supabaseKey);
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
