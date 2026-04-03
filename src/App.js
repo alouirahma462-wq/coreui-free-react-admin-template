953781e77a95293ecc67820e059927c8c9a4a8e1
alouirahma462-wq <alouirahma462@gmail.com>
2026-04-03 10:08:23 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,48 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 import Login from "./views/pages/login/Login.jsx";
 import ChangePassword from "./views/pages/ChangePassword.jsx";
+
+function Dashboard() {
+  return <h1>Dashboard</h1>;
+}
+
+// حماية الصفحات
+function ProtectedRoute({ children }) {
+  const user =
+    JSON.parse(localStorage.getItem("user")) ||
+    JSON.parse(sessionStorage.getItem("user"));
+
+  if (!user) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+}
 
 export default function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
+
       <Route path="/change-password" element={<ChangePassword />} />
 
-      {/* 🟢 خلّي الافتراضي login فقط */}
-      <Route path="/" element={<Login />} />
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
+
+      {/* default */}
+      <Route path="/" element={<Navigate to="/login" replace />} />
+      <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
   );
 }
+
 
 
 
