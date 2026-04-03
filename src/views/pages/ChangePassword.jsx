3ab4adb31fb066a4e1c96106bc2f00265a3875cf
alouirahma462-wq <alouirahma462@gmail.com>
2026-04-03 09:27:56 +0100

    Update ChangePassword.jsx

--- src/views/pages/ChangePassword.jsx
+++ src/views/pages/ChangePassword.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { supabase } from "../supabaseClient";
+import { supabase } from "../../supabaseClient";
+
 
 export default function ChangePasswordFirstLogin() {
   const [user, setUser] = useState(null);
