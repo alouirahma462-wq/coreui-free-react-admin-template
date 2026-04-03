8dcd7d848054842292d96e0e7a61e2237c3cbfa9
alouirahma462-wq <alouirahma462@gmail.com>
2026-04-03 02:41:57 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import Login from "./views/pages/login/Login.jsx";
 import ChangePassword from "./views/pages/changePassword/ChangePassword.jsx";
@@ -6,18 +6,14 @@ import Dashboard from "./views/pages/dashboard/Dashboard.jsx";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/change-password" element={<ChangePassword />} />
+      <Route path="/dashboard" element={<Dashboard />} />
 
-        <Route path="/login" element={<Login />} />
-        <Route path="/change-password" element={<ChangePassword />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-
-        {/* الافتراضي */}
-        <Route path="*" element={<Login />} />
-
-      </Routes>
-    </BrowserRouter>
+      {/* الافتراضي */}
+      <Route path="*" element={<Login />} />
+    </Routes>
   );
 }
 
