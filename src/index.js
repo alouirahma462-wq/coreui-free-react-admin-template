f41522c7021605e376e0bc0e321be0fff3b64256
alouirahma462-wq <alouirahma462@gmail.com>
2026-04-03 02:38:43 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import App from './App'
-import store from './store'
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </Provider>
-)
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
+
 
 
 
