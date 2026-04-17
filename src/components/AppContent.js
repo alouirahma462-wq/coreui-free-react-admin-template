a3d021f1c452a0f9fee179d55e255690d0d4c0e3
alouirahma462-wq <alouirahma462@gmail.com>
2026-04-17 14:39:00 +0100

    Update AppContent.js

--- src/components/AppContent.js
+++ src/components/AppContent.js
@@ -1,60 +1,35 @@
 import React, { Suspense } from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
-import { CContainer, CSpinner } from '@coreui/react'
+import { CSpinner } from '@coreui/react'
 
 import routes from '../routes'
 
-// 🔥 Layout components
-import { AppSidebar, AppHeader, AppFooter } from './index'
-
 const AppContent = () => {
   return (
-    <div className="app-layout">
+    <Suspense fallback={<CSpinner color="primary" />}>
 
-      {/* 🔥 SIDEBAR ثابت */}
-      <AppSidebar />
+      <Routes>
+        {routes.map((route, idx) => {
+          return (
+            route.element && (
+              <Route
+                key={idx}
+                path={route.path}
+                element={<route.element />}
+              />
+            )
+          )
+        })}
 
-      {/* 🔥 MAIN WRAPPER */}
-      <div className="wrapper d-flex flex-column min-vh-100">
+        {/* 🔥 REDIRECT */}
+        <Route path="/" element={<Navigate to="dashboard" replace />} />
 
-        {/* 🔥 HEADER ثابت */}
-        <AppHeader />
+      </Routes>
 
-        {/* 🔥 CONTENT AREA */}
-        <div className="body flex-grow-1 px-3">
-          <CContainer fluid>
-
-            <Suspense fallback={<CSpinner color="primary" />}>
-              <Routes>
-
-                {routes.map((route, idx) => {
-                  return (
-                    route.element && (
-                      <Route
-                        key={idx}
-                        path={route.path}
-                        element={<route.element />}
-                      />
-                    )
-                  )
-                })}
-
-                {/* 🔥 REDIRECT */}
-                <Route path="/" element={<Navigate to="dashboard" replace />} />
-
-              </Routes>
-            </Suspense>
-
-          </CContainer>
-        </div>
-
-        {/* 🔥 FOOTER ثابت */}
-        <AppFooter />
-
-      </div>
-    </div>
+    </Suspense>
   )
 }
 
 export default React.memo(AppContent)
+
 
