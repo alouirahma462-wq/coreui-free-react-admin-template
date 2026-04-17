bd49d85583eee5e9bed1e7952c827170ac94764a
alouirahma462-wq <alouirahma462@gmail.com>
2026-04-17 16:23:02 +0100

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -6,46 +6,38 @@ import autoprefixer from 'autoprefixer'
 export default defineConfig(() => {
   return {
     base: './',
-   build: {
-  outDir: 'dist',
-  emptyOutDir: true,
-},
+
+    plugins: [react()],
+
+    build: {
+      outDir: 'dist',
+      emptyOutDir: true,
+    },
 
     css: {
       postcss: {
-        plugins: [
-          autoprefixer({}), // add options if needed
-        ],
+        plugins: [autoprefixer()],
       },
     },
-    esbuild: {
-      loader: 'jsx',
-      include: /src\/.*\.jsx?$/,
-      exclude: [],
-    },
-    optimizeDeps: {
-      force: true,
-      esbuildOptions: {
-        loader: {
-          '.js': 'jsx',
-        },
-      },
-    },
-    plugins: [react()],
+
     resolve: {
-      alias: [
-        {
-          find: 'src/',
-          replacement: `${path.resolve(__dirname, 'src')}/`,
-        },
+      alias: {
+        src: path.resolve(__dirname, 'src'),
+      },
+      extensions: [
+        '.mjs',
+        '.js',
+        '.ts',
+        '.jsx',
+        '.tsx',
+        '.json',
+        '.scss',
       ],
-      extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.scss'],
     },
+
     server: {
       port: 3000,
-      proxy: {
-        // https://vitejs.dev/config/server-options.html
-      },
     },
   }
 })
+
