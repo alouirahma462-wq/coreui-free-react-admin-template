35954256f034ff8c0e66adb97fe3309aeb4c2518
alouirahma462-wq <alouirahma462@gmail.com>
2026-04-18 23:30:04 +0200

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,47 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'node:path'
-import autoprefixer from 'autoprefixer'
 
-export default defineConfig(() => {
-  return {
-    plugins: [react()],
+export default defineConfig({
+  plugins: [react()],
 
-    optimizeDeps: {
-      include: ['react', 'react-dom'],
-    },
+  optimizeDeps: {
+    include: ['react', 'react-dom'],
+  },
 
-    build: {
-      outDir: 'dist',
-      emptyOutDir: true,
-    },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
 
-    css: {
-      postcss: {
-        plugins: [autoprefixer()],
-      },
-    },
-
-    resolve: {
-      alias: {
-        src: path.resolve(__dirname, 'src'),
-      },
-      extensions: [
-        '.mjs',
-        '.js',
-        '.ts',
-        '.jsx',
-        '.tsx',
-        '.json',
-        '.scss',
-      ],
-    },
-
-    server: {
-      port: 3000,
-    },
-  }
+  server: {
+    port: 3000,
+  },
 })
+
 
 
 
