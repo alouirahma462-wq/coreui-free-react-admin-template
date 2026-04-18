5f241f362347e31ad881850a8ea2db934110595a
alouirahma462-wq <alouirahma462@gmail.com>
2026-04-18 18:09:37 +0200

    Update CourtLayout.jsx

--- src/layout/CourtLayout.jsx
+++ src/layout/CourtLayout.jsx
@@ -1,43 +1,34 @@
-import React from "react"
-import AppSidebar from "../components/AppSidebar"
-import AppHeader from "../components/AppHeader"
-import AppFooter from "../components/AppFooter"
+import React from "react";
+import AppSidebar from "../components/AppSidebar";
+import AppHeader from "../components/AppHeader";
+import AppFooter from "../components/AppFooter";
 
 const CourtLayout = ({ children }) => {
   return (
     <div className="app-layout">
-
-      {/* 🔥 SIDEBAR */}
       <AppSidebar type="court" />
 
-      {/* 🔥 MAIN */}
       <div
         className="wrapper d-flex flex-column min-vh-100"
         style={{
           background: "linear-gradient(135deg, #0f172a, #1e293b)",
-          color: "white"
+          color: "white",
         }}
       >
-
-        {/* 🔥 HEADER */}
         <AppHeader type="court" />
 
-        {/* 🔥 CONTENT */}
         <div
           className="body flex-grow-1 px-4 py-3"
-          style={{
-            backdropFilter: "blur(6px)"
-          }}
+          style={{ backdropFilter: "blur(6px)" }}
         >
           {children}
         </div>
 
-        {/* 🔥 FOOTER */}
         <AppFooter />
-
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CourtLayout
+export default CourtLayout;
+
