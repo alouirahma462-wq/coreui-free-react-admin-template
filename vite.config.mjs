1073950b1b70f2e607424dca204e6da5c4f899a2
alouirahma462-wq <alouirahma462@gmail.com>
2026-04-18 23:56:31 +0200

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,20 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-
-  optimizeDeps: {
-    include: ['react', 'react-dom'],
-  },
-
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-  },
-
-  server: {
-    port: 3000,
-  },
 })
+
 
 
 
