2820a170ff55e57fdf3ba7b7faf53e403c1e8273
alouirahma462-wq <alouirahma462@gmail.com>
2026-04-19 20:23:52 +0200

    Update AppSidebar.jsx

--- src/components/AppSidebar.jsx
+++ src/components/AppSidebar.jsx
@@ -1,24 +1,40 @@
-import React from "react"
-import { useSelector } from "react-redux"
-import courtNav from "../navigation/courtNav"
-import inspectionNav from "../navigation/inspectionNav"
-import AppSidebarNav from "./AppSidebarNav"
+import React from "react";
+import { useSelector } from "react-redux";
+import courtNav from "../navigation/courtNav";
+import inspectionNav from "../navigation/inspectionNav";
+import AppSidebarNav from "./AppSidebarNav";
 
 const AppSidebar = ({ type }) => {
-  const sidebarShow = useSelector((state) => state.sidebarShow)
+  const sidebarShow = useSelector(
+    (state) => state.sidebarShow ?? true
+  );
 
-  const navigation = type === "court" ? courtNav : inspectionNav
+  if (!sidebarShow) return null;
 
-  if (!sidebarShow) return null
+  const navigation =
+    type === "court"
+      ? courtNav
+      : type === "inspection"
+      ? inspectionNav
+      : [];
 
   return (
-    <div className="sidebar-custom">
+    <aside
+      className="sidebar-custom"
+      style={{
+        width: "260px",
+        background: "#0f172a",
+        color: "white",
+        minHeight: "100vh",
+      }}
+    >
       <AppSidebarNav items={navigation} />
-    </div>
-  )
-}
+    </aside>
+  );
+};
 
-export default AppSidebar
+export default AppSidebar;
+
 
 
 
