79451ba798fe7421518cf2f1fa9e467dc55d9e76
alouirahma462-wq <alouirahma462@gmail.com>
2026-04-19 19:08:00 +0200

    Update AppSidebar.jsx

--- src/components/AppSidebar.jsx
+++ src/components/AppSidebar.jsx
@@ -1,84 +1,25 @@
 import React from "react"
-import { useDispatch, useSelector } from "react-redux"
-
-import {
-  CSidebar,
-  CSidebarBrand,
-  CSidebarFooter,
-  CSidebarHeader,
-  CSidebarToggler,
-  CCloseButton
-} from "@coreui/react"
-
-import CIcon from "@coreui/icons-react"
-
-import logo from "../assets/brand/logo"
-import sygnet from "../assets/brand/sygnet"
-
-import AppSidebarNav from "./AppSidebarNav"
-
-// 🔥 أهم تغيير: بدل _nav
+import { useSelector } from "react-redux"
 import courtNav from "../navigation/courtNav"
 import inspectionNav from "../navigation/inspectionNav"
+import AppSidebarNav from "./AppSidebarNav"
 
 const AppSidebar = ({ type }) => {
-  const dispatch = useDispatch()
-
-  const unfoldable = useSelector((state) => state.sidebarUnfoldable)
   const sidebarShow = useSelector((state) => state.sidebarShow)
 
-  // 🔥 اختيار القائمة حسب الداشبورد
-  const navigation =
-    type === "court"
-      ? courtNav
-      : inspectionNav
+  const navigation = type === "court" ? courtNav : inspectionNav
+
+  if (!sidebarShow) return null
 
   return (
-    <CSidebar
-      className="border-end"
-      colorScheme="dark"
-      position="fixed"
-      unfoldable={unfoldable}
-      visible={sidebarShow}
-      onVisibleChange={(visible) =>
-        dispatch({ type: "set", sidebarShow: visible })
-      }
-    >
-      {/* HEADER */}
-      <CSidebarHeader className="border-bottom">
-        <CSidebarBrand to="/">
-          <CIcon customClassName="sidebar-brand-full" icon={logo} height={32} />
-          <CIcon customClassName="sidebar-brand-narrow" icon={sygnet} height={32} />
-        </CSidebarBrand>
-
-        <CCloseButton
-          className="d-lg-none"
-          dark
-          onClick={() =>
-            dispatch({ type: "set", sidebarShow: false })
-          }
-        />
-      </CSidebarHeader>
-
-      {/* NAVIGATION */}
+    <div className="sidebar-custom">
       <AppSidebarNav items={navigation} />
-
-      {/* FOOTER */}
-      <CSidebarFooter className="border-top d-none d-lg-flex">
-        <CSidebarToggler
-          onClick={() =>
-            dispatch({
-              type: "set",
-              sidebarUnfoldable: !unfoldable
-            })
-          }
-        />
-      </CSidebarFooter>
-    </CSidebar>
+    </div>
   )
 }
 
-export default React.memo(AppSidebar)
+export default AppSidebar
+
 
 
 
