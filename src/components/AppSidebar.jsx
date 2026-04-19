bd3ad17c6b671faa027372ef8f739f2d6822a89f
alouirahma462-wq <alouirahma462@gmail.com>
2026-04-19 18:29:44 +0200

    Update AppSidebar.jsx

--- src/components/AppSidebar.jsx
+++ src/components/AppSidebar.jsx
@@ -16,13 +16,22 @@ import logo from "../assets/brand/logo"
 import sygnet from "../assets/brand/sygnet"
 
 import AppSidebarNav from "./AppSidebarNav"
-import navigation from "../_nav"
 
-const AppSidebar = () => {
+// 🔥 أهم تغيير: بدل _nav
+import courtNav from "../navigation/courtNav"
+import inspectionNav from "../navigation/inspectionNav"
+
+const AppSidebar = ({ type }) => {
   const dispatch = useDispatch()
 
   const unfoldable = useSelector((state) => state.sidebarUnfoldable)
   const sidebarShow = useSelector((state) => state.sidebarShow)
+
+  // 🔥 اختيار القائمة حسب الداشبورد
+  const navigation =
+    type === "court"
+      ? courtNav
+      : inspectionNav
 
   return (
     <CSidebar
@@ -35,6 +44,7 @@ const AppSidebar = () => {
         dispatch({ type: "set", sidebarShow: visible })
       }
     >
+      {/* HEADER */}
       <CSidebarHeader className="border-bottom">
         <CSidebarBrand to="/">
           <CIcon customClassName="sidebar-brand-full" icon={logo} height={32} />
@@ -50,8 +60,10 @@ const AppSidebar = () => {
         />
       </CSidebarHeader>
 
+      {/* NAVIGATION */}
       <AppSidebarNav items={navigation} />
 
+      {/* FOOTER */}
       <CSidebarFooter className="border-top d-none d-lg-flex">
         <CSidebarToggler
           onClick={() =>
