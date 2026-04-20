7e30e4494b1ab2e063fa3d66deafaccd96aff1df
alouirahma462-wq <alouirahma462@gmail.com>
2026-04-20 13:45:35 +0100

    Update DocsComponents.jsx

--- src/components/DocsComponents.jsx
+++ src/components/DocsComponents.jsx
@@ -1,30 +1,31 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 
-import ComponentsImg from 'src/assets/images/components.webp'
-
 const DocsComponents = (props) => (
   <div className="bg-primary bg-opacity-10 border border-2 border-primary rounded mb-4">
     <div className="row d-flex align-items-center p-3 px-xl-4 flex-xl-nowrap">
+
       <div className="col-xl-auto col-12 d-none d-xl-block p-0">
         <img
           className="img-fluid"
-          src={ComponentsImg}
+          src="/components.webp"
           width="160px"
           height="160px"
           alt="CoreUI PRO hexagon"
         />
       </div>
+
       <div className="col-md col-12 px-lg-4">
         Our Admin Panel isn’t just a mix of third-party components. It’s{' '}
         <strong>
           the only open-source React dashboard built on a professional, enterprise-grade UI
           Components Library
         </strong>
-        . This component is part of this library, and we present only the basic usage of it here. To
-        explore extended examples, detailed API documentation, and customization options, refer to
-        our docs.
+        . This component is part of this library, and we present only the basic usage of it here.
+        To explore extended examples, detailed API documentation, and customization options, refer
+        to our docs.
       </div>
+
       <div className="col-md-auto col-12 mt-3 mt-lg-0">
         <a
           className="btn btn-primary text-nowrap text-white"
@@ -35,6 +36,7 @@ const DocsComponents = (props) => (
           Explore Documentation
         </a>
       </div>
+
     </div>
   </div>
 )
@@ -44,3 +46,4 @@ DocsComponents.propTypes = {
 }
 
 export default DocsComponents
+
