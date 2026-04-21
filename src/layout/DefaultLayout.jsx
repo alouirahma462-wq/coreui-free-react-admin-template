ee5ff3447bc4282165ef6b674f81063102089acf
alouirahma462-wq <alouirahma462@gmail.com>
2026-04-21 23:05:21 +0200

    Update DefaultLayout.jsx

--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -1,31 +1,84 @@
-import React from 'react'
-import { AppContent, AppSidebar, AppFooter, AppHeader } from '../components/index'
+import React from "react";
+import { AppContent, AppSidebar, AppFooter, AppHeader } from "../components/index";
 
 const DefaultLayout = () => {
   return (
-    <div className="wrapper d-flex flex-column min-vh-100">
+    <div style={styles.wrapper}>
 
-      {/* 🔥 Sidebar لازم يكون داخل wrapper */}
-      <AppSidebar />
+      {/* Sidebar */}
+      <aside style={styles.sidebar}>
+        <AppSidebar />
+      </aside>
 
-      {/* 🔥 Main Area */}
-      <div className="body flex-grow-1 d-flex flex-column min-vh-100">
+      {/* Main */}
+      <div style={styles.main}>
 
         {/* Header */}
-        <AppHeader />
+        <header style={styles.header}>
+          <AppHeader />
+        </header>
 
         {/* Content */}
-        <div className="flex-grow-1">
+        <main style={styles.content}>
           <AppContent />
-        </div>
+        </main>
 
         {/* Footer */}
-        <AppFooter />
+        <footer style={styles.footer}>
+          <AppFooter />
+        </footer>
 
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default DefaultLayout
+export default DefaultLayout;
+
+/* 🔥 CSS FIX ROOT */
+const styles = {
+  wrapper: {
+    display: "flex",
+    height: "100vh",
+    width: "100%",
+    overflow: "hidden",
+  },
+
+  sidebar: {
+    width: "260px",
+    height: "100vh",
+    flexShrink: 0,
+    background: "#111827",
+    overflowY: "auto",
+  },
+
+  main: {
+    flex: 1,
+    display: "flex",
+    flexDirection: "column",
+    height: "100vh",
+    overflow: "hidden",
+  },
+
+  header: {
+    height: "60px",
+    flexShrink: 0,
+    background: "#1e3a8a",
+    zIndex: 10,
+  },
+
+  content: {
+    flex: 1,
+    overflowY: "auto",
+    background: "#f3f4f6",
+    padding: "15px",
+  },
+
+  footer: {
+    height: "40px",
+    flexShrink: 0,
+    background: "#111827",
+  },
+};
+
 
