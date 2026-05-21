34aa7f762c6b150df2794dafd3feceefaf3614ef
alouirahma462-wq <alouirahma462@gmail.com>
2026-05-21 22:06:41 +0200

    Create embeddings.js

---
+++ src/modules/cases/ai/rag/embeddings.js
@@ -0,0 +1,12 @@
+import { openai } from "../client.js"
+
+// 🧠 تحويل النص إلى vector
+export const createEmbedding = async (text) => {
+
+  const response = await openai.embeddings.create({
+    model: "text-embedding-3-small",
+    input: text
+  })
+
+  return response.data[0].embedding
+}
