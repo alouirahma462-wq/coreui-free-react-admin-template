ff33c595a827da82b210e2c6fc4730773ada6d3c
alouirahma462-wq <alouirahma462@gmail.com>
2026-06-02 15:46:15 +0100

    Update client.js

--- src/modules/cases/ai/client.js
+++ src/modules/cases/ai/client.js
@@ -6,5 +6,5 @@ dotenv.config()
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY)
 
 export const ai = genAI.getGenerativeModel({
-  model: "gemini-1.5-pro"
+  model: "gemini-1.5-flash-002"
 })
