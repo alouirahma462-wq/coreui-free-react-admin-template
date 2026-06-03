1f01a3993aaf11b0320c7045d58615a9ca87aaff
alouirahma462-wq <alouirahma462@gmail.com>
2026-06-03 23:10:36 +0200

    Update client.js

--- src/modules/cases/ai/client.js
+++ src/modules/cases/ai/client.js
@@ -6,7 +6,7 @@ export const ai = async (prompt) => {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": `Bearer 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`
+     "Authorization": `Bearer ${import.meta.env.VITE_GROQ_API_KEY}`
       },
       body: JSON.stringify({
         model: "llama-3.1-8b-instant",
